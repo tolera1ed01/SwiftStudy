4905edd19321c3920ee27a38113cf61c0a423a35
tolera1ed01 <tolera1ed@gmail.com>
2025-01-21 23:55:31 +0000

    Progress 21/01/25

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, SafeAreaView, TextInput } from "react-native";
 import styles from "../stylesheet";
-
-const LoginImage = require("../assets/images/LoginImage.jpg");
 
 
 
@@ -11,15 +9,23 @@ const LoginImage = require("../assets/images/LoginImage.jpg");
 
 const LoginScreen = () => {
   return (
-    <View style={{flex: 1, justifyContent: "center", alignContent: "center"}}>
-      <Text style={styles.appName}>Welcome to SwiftStudy!</Text>
-      <Text style={{position: "relative", bottom: 275, right: 30, fontSize: 45, fontWeight: "bold", fontFamily: "",}}>Login</Text>
-      <View style= {styles.imageContainer}>
-        <Image source={LoginImage} style={{ width:200, height:200, position: "relative",  }} />
-      </View>
-    </View>
-  )
+      <SafeAreaView style={styles.container}> 
+        <Text style={styles.appName}>Welcome to SwiftStudy!</Text>
+        <Text style={styles.loginTitle}>Login</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputBox}
+            placeholder="Username"
+          />
+          <TextInput
+            style={styles.inputBox}
+            placeholder="Password"
+          />
+        </View>
+      </SafeAreaView>
+  );
 }
+
 
 
 export default LoginScreen;
--- stylesheet.js
+++ stylesheet.js
@@ -1,17 +1,20 @@
+import { Dimensions } from 'react-native';
 import { StyleSheet, Text, View } from "react-native";
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    width: windowWidth * 1,
+    height: windowHeight * 1.2,
   },
   appName: {
-    position: "relative",
-    bottom: 300,
-    
-
+    bottom: 200,
     fontSize: 24,
     fontWeight: "500",
     fontFamily: "",
@@ -26,6 +29,33 @@ const styles = StyleSheet.create({
     bottom: 400,
     left: 0,
     right: 0,
+  },
+  loginTitle: {
+    position: "relative", 
+    marginTop: "50%",
+    bottom: 225, 
+    right: 100, 
+    fontSize: 45, 
+    fontWeight: "bold", 
+    fontFamily: "",
+  },
+  inputBox: {
+    backgroundColor: "#f5f5f5",
+    borderColor: "#ccc",
+    padding: 10,
+    borderRadius: 5,
+    width: "90%",
+    height: "50",
+    marginTop: 20,
+  },
+  inputContainer: {
+    position: "relative",
+    bottom: 200,
+    width: "350",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+
   },
 });
 
