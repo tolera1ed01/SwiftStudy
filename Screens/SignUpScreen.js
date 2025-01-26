dd834efd8e41ee503ccefc3b7c79015a7201e454
tolera1ed01 <tolera1ed@gmail.com>
2025-01-26 22:57:04 +0000

    MovingBetweenScreens

--- Screens/SignUpScreen.js
+++ Screens/SignUpScreen.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { View, Text, SafeAreaView, TextInput, TouchableOpacity } from "react-native";
 import styles from "../stylesheet";
-import BackArrow from "../assets/images/arrow-left.svg"
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { useNavigation } from "@react-navigation/native";
 
 const SignUpScreen = () => {
+
+    const navigation = useNavigation();
+
+
   return (
     <SafeAreaView style={styles.container}>
-      
-
+      <TouchableOpacity 
+      onPress={() => { navigation.goBack("LoginScreen") }}>
+      <AntDesign name="arrowleft" size={30} color="#2F92B8" />
+    </TouchableOpacity>
     </SafeAreaView>
   );
 }
