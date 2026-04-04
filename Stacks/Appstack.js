71f1b43c9a3bc624231efcc41ac8fced5e9e38a4
tolera1ed01 <tolera1ed@gmail.com>
2026-04-04 22:05:31 +0100

    comment

--- Stacks/Appstack.js
+++ Stacks/Appstack.js
@@ -13,11 +13,11 @@ const Stack = createStackNavigator();
 
 
 export default function AppStack() {
-
+// any screens here in the stack are screens that cant be directly navigated to through the navigation bar of the homescreen and thus needs an alternative location(here) to be accessed
   return(
     <>
       <StatusBar barStyle="light-content" 
-      translucent={true} 
+      translucent={true}  
       backgroundColor="transparent"
       /> 
       <NavigationContainer theme={SwiftstudyTheme}> 
@@ -26,8 +26,7 @@ export default function AppStack() {
           <Stack.Screen name="SignUpScreen" component={SignUpScreen} options={{ headerShown: false }} />
           <Stack.Screen name="HomeScreen" component={Navbar} options={{ headerShown: false }} />
           <Stack.Screen name="FlashcardsScreen" component={FlashcardsScreen}/>
-          <Stack.Screen name="ReviseScreen" component={ReviseScreen}/>
-
+          <Stack.Screen name="ReviseScreen" component={ReviseScreen}/> 
         </Stack.Navigator>
       </NavigationContainer>
     </>
